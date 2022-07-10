556c803a1782d00a0571910d6817764378621eb0
pgeovany <pgeovany@dcc.ufrj.br>
2022-07-10 15:24:52 -0300

    feat: implement a function to, every twenty-four hours, delete every session that was created over an hour from then.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express, { json } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import router from './routes/router.js';
+import deleteGhostSessions from './utils/session/deleteGhostSessions.js';
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const server = express();
 server.use([cors(), json()]);
 
 server.use(router);
+
+deleteGhostSessions();
 
 server.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
